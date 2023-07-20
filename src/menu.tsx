0fb33ae19d24b7af103370df39bec4a93f76555c
shuzhiwen <yuwen4012@gmail.com>
2023-07-20 15:18:54 +0800

    feat: perfect game guide

--- src/menu.tsx
+++ src/menu.tsx
@@ -1,6 +1,7 @@
+import TapHere from '@assets/image/tap-here.jpeg'
 import {SportsEsports} from '@mui/icons-material'
 import {IconButton, Menu, MenuItem, Stack, Typography} from '@mui/material'
-import {isMobile} from '@utils/chaos'
+import {isMobile} from '@utils'
 import React from 'react'
 import {useLocation, useNavigate} from 'react-router-dom'
 
@@ -10,28 +11,37 @@ const MenuDict: Record<string, string> = {
 }
 
 export function GameMenu() {
-  const mobile = isMobile()
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null)
   const open = Boolean(anchorEl)
   const navigate = useNavigate()
   const {pathname} = useLocation()
-  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-    setAnchorEl(event.currentTarget)
-  }
+  const isRootPage = pathname === '/'
   const handleNavigate = (path: string) => {
     setAnchorEl(null)
     navigate(path)
   }
 
-  return mobile ? null : (
+  return isMobile() && !isRootPage ? null : (
     <Stack>
       <Stack direction="row" spacing={1} alignItems="center">
-        <IconButton onClick={handleClick} sx={{width: 'fit-content'}}>
+        <IconButton onClick={(e) => setAnchorEl(e.currentTarget)}>
           <SportsEsports />
         </IconButton>
         <Typography color="black">{MenuDict[pathname]}</Typography>
+        {isRootPage && (
+          <img
+            src={TapHere}
+            style={{
+              position: 'absolute',
+              rotate: '-30deg',
+              zIndex: -1,
+              left: 30,
+              top: 0,
+            }}
+          />
+        )}
       </Stack>
-      <Menu anchorEl={anchorEl} onClose={handleClick} open={open}>
+      <Menu anchorEl={anchorEl} onClose={() => setAnchorEl(null)} open={open}>
         {Object.entries(MenuDict).map(([path, title]) => (
           <MenuItem key={path} onClick={() => handleNavigate(path)}>
             {title}
--- src/module.d.ts
+++ src/module.d.ts
@@ -3,6 +3,11 @@ declare module '*.jpg' {
   export default image
 }
 
+declare module '*.jpeg' {
+  const image: string
+  export default image
+}
+
 declare module '*.mp3' {
   const audio: string
   export default audio
