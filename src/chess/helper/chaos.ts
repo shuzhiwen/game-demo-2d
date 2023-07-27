9b25a358dbf342719da71b50e7813ba4bbb4f29f
shuzhiwen <yuwen4012@gmail.com>
2023-07-27 11:14:44 +0800

    fix(chess): chinese invitation code does not work

--- src/chess/helper/chaos.ts
+++ src/chess/helper/chaos.ts
@@ -2,12 +2,13 @@ import {ElSource} from 'awesome-chart/dist/types'
 
 export function encodeInviteUrl(inviteCode: string) {
   const pathname = window.location.pathname.split('/').find(Boolean)
-  return `${window.location.origin}/${pathname}/login?code=${btoa(inviteCode!)}`
+  const base64 = btoa(encodeURI(inviteCode))
+  return `${window.location.origin}/${pathname}/login?code=${base64}`
 }
 
 export function decodeInviteUrl(inviteCode: string) {
   try {
-    return atob(inviteCode!)
+    return decodeURI(atob(inviteCode))
   } catch {
     return null
   }
